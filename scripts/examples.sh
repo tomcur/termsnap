1bae371b481f394259b146ef0ead4eec4e3332b5
Thomas Churchman <thomas@kepow.org>
2024-06-19 17:37:35 +0200

    ci: explicitly build binary

--- scripts/examples.sh
+++ scripts/examples.sh
@@ -2,9 +2,13 @@
 
 # This script expects to be run from the repository's root directory.
 
-cargo run -- -o ./media/ls.svg -l 12 -c 60 -- ls -l --color=always
+cargo build --release
 
-cargo run -- -o ./media/nvim.svg -l 12 -c 60 -- nvim --clean \
+export PATH="$PWD/target/release":$PATH
+
+termsnap -o ./media/ls.svg -l 12 -c 60 -- ls -l --color=always
+
+termsnap -o ./media/nvim.svg -l 12 -c 60 -- nvim --clean \
     -c "set number" \
     -c "syntax enable" \
     -c "lua vim.defer_fn(function() vim.cmd('q') end, 0)" \
