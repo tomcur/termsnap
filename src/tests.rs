b8e65a16dd520f1ee5b7385cbf606d8133224615
Thomas Churchman <thomas@kepow.org>
2024-06-30 17:47:41 +0200

    test: add test calling out to bash

---
+++ src/tests.rs
@@ -0,0 +1,23 @@
+use clap::Parser;
+
+use super::{run, Cli};
+
+#[test]
+fn bash_echo() {
+    let cli = Cli::parse_from([
+        "termsnap",
+        "-l",
+        "20",
+        "-c",
+        "80",
+        "--",
+        "bash",
+        "-c",
+        "echo 'hello, world'",
+    ]);
+
+    let screen = run(cli).unwrap();
+    let content: String = screen.cells().map(|c| c.c).collect();
+
+    assert!(content.starts_with("hello, world"));
+}
