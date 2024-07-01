tomcur/termsnap

commit fc27cd6e5f069a21483b234afcbaabec196a1ac5

test: add test calling out to bash

src/main.rs
@@ -25,6 +25,9 @@ mod poll;
 mod ringbuffer;
 use ringbuffer::{IoResult, Ringbuffer};
 
+#[cfg(test)]
+mod tests;
+
 const DEFAULT_NUM_LINES: u16 = 24;
 const DEFAULT_NUM_COLUMNS: u16 = 80;
 
@@ -344,8 +347,32 @@ fn from_read(read: &mut impl Read, lines: u16, columns: u16) -> anyhow::Result<S
 }
 
 fn main() -> anyhow::Result<()> {
-    let cli = Cli::parse();
+    let mut cli = Cli::parse();
+    let out = cli.out.take();
+    let screen = run(cli)?;
 
+    let fonts = &[
+        "ui-monospace",
+        "Consolas",
+        "Liberation Mono",
+        "Source Code Pro",
+    ];
+
+    if let Some(out) = out {
+        let mut file = std::fs::OpenOptions::new()
+            .write(true)
+            .truncate(true)
+            .create(true)
+            .open(out)?;
+        write!(file, "{}", screen.to_svg(fonts))?;
+    } else {
+        println!("{}", screen.to_svg(fonts))
+    }
+
+    Ok(())
+}
+
+fn run(cli: Cli) -> anyhow::Result<Screen> {
     if cli.interactive {
         if cli.out.is_none() {
             anyhow::bail!("`--interactive` is set but no SVG output file is specified in `--out`. See `termsnap --help`.");
@@ -433,23 +460,5 @@ fn main() -> anyhow::Result<()> {
         }
     };
 
-    let fonts = &[
-        "ui-monospace",
-        "Consolas",
-        "Liberation Mono",
-        "Source Code Pro",
-    ];
-
-    if let Some(out) = cli.out {
-        let mut file = std::fs::OpenOptions::new()
-            .write(true)
-            .truncate(true)
-            .create(true)
-            .open(out)?;
-        write!(file, "{}", screen.to_svg(fonts))?;
-    } else {
-        println!("{}", screen.to_svg(fonts))
-    }
-
-    Ok(())
+    Ok(screen)
 }

src/tests.rs
@@ -1,0 +1,23 @@
+use clap::Parser;
+
+use super::{run, Cli};
+
+#[test]
+fn bash_echo() {
+    let cli = Cli::parse_from([
+        "termsnap",
+        "-l",
+        "20",
+        "-c",
+        "80",
+        "--",
+        "bash",
+        "-c",
+        "echo 'hello, world'",
+    ]);
+
+    let screen = run(cli).unwrap();
+    let content: String = screen.cells().map(|c| c.c).collect();
+
+    assert!(content.starts_with("hello, world"));
+}

commit 501e6293849df204d8427e8888876a6feec0238f

fix: separate main logic and CLI arguments consistency checks
fixes regression caused by fc27cd6e5f069a21483b234afcbaabec196a1ac5

src/main.rs
@@ -357,12 +357,34 @@ fn from_read(read: &mut impl Read, lines: u16, columns: u16) -> anyhow::Result<S
 
 fn main() -> anyhow::Result<()> {
     let mut cli = Cli::parse();
+
+    let mut parent_stdin = std::io::stdin().lock();
+    let mut parent_stdout = std::io::stdout().lock();
+
+    if cli.interactive {
+        if cli.out.is_none() {
+            anyhow::bail!("`--interactive` is set but no SVG output file is specified in `--out`. See `termsnap --help`.");
+        }
+
+        if cli.lines.is_some() || cli.columns.is_some() {
+            eprintln!("Warning: Setting `--lines` and `--columns` has no effect when `--interactive` is set");
+        }
+
+        if !parent_stdin.as_fd().is_terminal() {
+            eprintln!("Warning: `--interactive` is set, but stdin is not a tty")
+        }
+
+        if !parent_stdin.as_fd().is_terminal() {
+            eprintln!("Warning: `--interactive` is set, but stdout is not a tty")
+        }
+    }
+
+    if cli.command.is_none() && parent_stdin.as_fd().is_terminal() {
+        anyhow::bail!("No command given to execute. See 'termsnap --help'. To use Termsnap without it executing a command, you can pipe data into Termsnap.");
+    }
+
     let out = cli.out.take();
-    let screen = run(
-        cli,
-        &mut std::io::stdin().lock(),
-        &mut std::io::stdout().lock(),
-    )?;
+    let screen = run(cli, &mut parent_stdin, &mut parent_stdout)?;
 
     let fonts = &[
         "ui-monospace",
@@ -390,24 +412,6 @@ where
     I: Read + AsFd,
     O: Write + AsFd,
 {
-    if cli.interactive {
-        if cli.out.is_none() {
-            anyhow::bail!("`--interactive` is set but no SVG output file is specified in `--out`. See `termsnap --help`.");
-        }
-
-        if cli.lines.is_some() || cli.columns.is_some() {
-            eprintln!("Warning: Setting `--lines` and `--columns` has no effect when `--interactive` is set");
-        }
-
-        if !parent_stdin.as_fd().is_terminal() {
-            eprintln!("Warning: `--interactive` is set, but stdin is not a tty")
-        }
-
-        if !parent_stdin.as_fd().is_terminal() {
-            eprintln!("Warning: `--interactive` is set, but stdout is not a tty")
-        }
-    }
-
     let (lines, columns) = if cli.interactive {
         termios::tcgetwinsize(std::io::stdout())
             .map(|winsize| (winsize.ws_row, winsize.ws_col))
@@ -466,13 +470,7 @@ where
                 non_interactive(parent_stdin, &mut pty, lines, columns)?
             }
         }
-        None => {
-            if parent_stdin.as_fd().is_terminal() {
-                anyhow::bail!("No command given to execute. See 'termsnap --help'. To use Termsnap without it executing a command, you can pipe data into Termsnap.");
-            }
-
-            from_read(parent_stdin, lines, columns)?
-        }
+        None => from_read(parent_stdin, lines, columns)?,
     };
 
     Ok(screen)